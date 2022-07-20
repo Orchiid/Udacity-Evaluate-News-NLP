5e505239c7c54351206675fd09538b5f8ff57d2c
Orchiid <joannatolofari@gmail.com>
2022-07-20 14:17:40 +0100

    readme

--- src/server/mockAPI.js
+++ src/server/mockAPI.js
@@ -38,19 +38,3 @@
 
 
 
-
-
-// const blogUri = document.getElementById('URL');
-// const blogText = document.querySelector('#text');
-// const apiData = {
-//     baseUrl : 'https://api.meaningcloud.com/sentiment-2.1',
-//     apiKey : "64fed33bfad5b85b14021447bcd0a9be",
-//     blogUrl : blogUri.value,
-//     blogTxt : blogText.value,
-// }
-// const {baseUrl, blogUrl ,apiKey, blogTxt} = apiData;
-// const apiUrl = `${baseUrl}?key=${apiKey}&url=${blogUrl}&txt='${blogTxt}'&lang=en`
-
-// fetch(apiUrl)
-//     .then((data) => 
-//          console.log(data.json()));
